c626e57f3d84cfcfe95513fc962b4617a2cc6e61
Seokju Na <seokmaTD@gmail.com>
2016-07-01 14:39:49 +0900

    add coverage using istanbul

--- karma.config.js
+++ karma.config.js
@@ -1,16 +1,10 @@
+var istanbul = require('browserify-istanbul');
+
 module.exports = function (config) {
     config.set({
-
-        // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
-
-
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['browserify', 'jasmine'],
 
-
-        // list of files / patterns to load in the browser
         files: [
             'src/vendor/karma-read-json/karma-read-json.js',
             'src/vendor/angular/angular.js',
@@ -27,60 +21,44 @@ module.exports = function (config) {
             'karma-browserify',
             'karma-jasmine',
             'karma-phantomjs-launcher',
-            'karma-mocha-reporter'
+            'karma-mocha-reporter',
+            'karma-coverage'
         ],
 
-        // list of files to exclude
         exclude: [
             'build/',
             'node_modules/'
         ],
 
-
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
             'src/js/main.js': ['browserify']
         },
 
         browserify: {
             debug: true,
-            transform: ['bulkify']
+            transform: ['bulkify', istanbul({
+                ignore: [
+                    '**/node_modules/**',
+                    '**/build/**',
+                    '**/tests/unit/**',
+                    '**/src/js/**/index.js',
+                    '**/src/js/main.js',
+                    '**/src/js/utils/constants.js'
+                ]
+            })]
         },
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['mocha'],
-
-
-        // web server port
-        port: 9876,
-
-
-        // enable / disable colors in the output (reporters and logs)
+        reporters: ['mocha', 'coverage'],
+        port: 9877,
         colors: true,
-
-        // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
-
         logLevel: config.LOG_INFO,
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['PhantomJS'],
-
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
         singleRun: false,
-
-        // Concurrency level
-        // how many browser should be started simultaneous
         concurrency: Infinity,
-
-        client: {
-            captureConsole: true
+        coverageReporter: {
+            type : 'html',
+            dir : 'tests/unit/coverage/'
         }
     });
 };
