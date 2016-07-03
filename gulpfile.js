473625524ee63f7105451dce7d1b59a56b9eaf45
Seokju Na <seokmaTD@gmail.com>
2016-07-03 22:19:30 +0900

    make deploy settup: build static files minified and upload by surge

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,12 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var LessPluginAutoPrefix = require('less-plugin-autoprefix');
 var autoprefix = new LessPluginAutoPrefix();
+var minifyCss = require('gulp-minify-css');
 
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
 var bulkify = require('bulkify');
 
 var connect = require('gulp-connect');
@@ -52,11 +54,32 @@ gulp.task('build-js', ['clean-js'], function () {
         .pipe(connect.reload());
 });
 
+gulp.task('build-js:minify', ['clean-js'], function () {
+    browserify(paths.src.jsMain)
+        .transform(bulkify)
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.build.js))
+        .pipe(connect.reload());
+});
+
 gulp.task('build-styles', ['clean-styles'], function () {
     return gulp.src(paths.src.stylesMain)
         .pipe(less({
             plugins: [autoprefix]
         }))
+        .pipe(gulp.dest(paths.build.css))
+        .pipe(connect.reload());
+});
+
+gulp.task('build-styles:minify', ['clean-styles'], function () {
+    return gulp.src(paths.src.stylesMain)
+        .pipe(less({
+            plugins: [autoprefix]
+        }))
+        .pipe(minifyCss())
         .pipe(gulp.dest(paths.build.css))
         .pipe(connect.reload());
 });
@@ -136,4 +159,5 @@ gulp.task('connect', function () {
 
 
 gulp.task('build', ['build-js', 'build-styles', 'build-indexHtml', 'build-views', 'build-vendor', 'build-assets']);
+gulp.task('build:production', ['build-js:minify', 'build-styles:minify', 'build-indexHtml', 'build-views', 'build-vendor', 'build-assets']);
 gulp.task('default', ['build', 'connect', 'watch']);
